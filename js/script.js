4285a16444269a3033e73e12fc96024040e8b750
smulczynskasia <107355316+smulczynskasia@users.noreply.github.com>
2022-07-03 18:17:11 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -16,7 +16,7 @@ for(let activeLink of activeLinks){
 
 /* add class 'active' to the clicked link */
 
-console.log('clickedElement:', clickedElement);
+  console.log('clickedElement:', clickedElement);
 
 /* remove class 'active' from all articles */
 
