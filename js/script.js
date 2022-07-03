ac070058444eef0689e1a09f9507a88b66cdd049
smulczynskasia <107355316+smulczynskasia@users.noreply.github.com>
2022-07-03 18:23:14 +0200

    Add active to link

--- js/script.js
+++ js/script.js
@@ -14,6 +14,7 @@ for(let activeLink of activeLinks){
 
 /* add class 'active' to the clicked link */
   
+  clickedElement.add('active');
   console.log('clickedElement:', clickedElement);
 
 /* remove class 'active' from all articles */
