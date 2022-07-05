282406dbd74064840cb810f079e18158c20b056b
smulczynskasia <107355316+smulczynskasia@users.noreply.github.com>
2022-07-05 19:05:04 +0200

    Add active

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
 'use strict';
 
 function titleClickHandler(event){
+  const clickedElement = this;
   console.log('Link was clicked!');
-  console.log(event);
 
   /* remove class 'active' from all article links  */
 
@@ -14,6 +14,9 @@ for(let activeLink of activeLinks){
   
   /* add class 'active' to the clicked link */
 
+  clickedElement.add('active');
+  console.log('clickedElement:', clickedElement);
+  
   /* remove class 'active' from all articles */
 
   const activeArticles = document.querySelectorAll('.posts .active');
