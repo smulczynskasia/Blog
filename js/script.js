4b9b1939e89141eba5c5b218bf143a5929002ff7
smulczynskasia <107355316+smulczynskasia@users.noreply.github.com>
2022-07-05 18:47:32 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -3,29 +3,18 @@
 function titleClickHandler(event){
   console.log('Link was clicked!');
 
-/* remove class 'active' from all article links */
+  /* remove class 'active' from all article links  */
 
-const activeLinks = document.querySelectorAll('.titles a.active');
+  /* add class 'active' to the clicked link */
 
-for(let activeLink of activeLinks){
-  activeLink.classList.remove('active');
+  /* remove class 'active' from all articles */
+
+  /* get 'href' attribute from the clicked link */
+
+  /* find the correct article using the selector (value of 'href' attribute) */
+
+  /* add class 'active' to the correct article */
 }
-
-/* add class 'active' to the clicked link */
-  
-/* remove class 'active' from all articles */
-
-const activeArticles = document.querySelectorAll('.posts .active');
-
-for(let activeArticle of activeArticles) {
-  activeArticle.classList.remove('active');
-}
-
-/* get 'href' attribute from the clicked link */
-
-/* find the correct article using the selector (value of 'href' attribute) */
-  
-/* add class 'active' to the correct article */
 
 const links = document.querySelectorAll('.titles a');
 
