f38807168782c94b7d3b646638adbfaa43b1ca18
smulczynskasia <107355316+smulczynskasia@users.noreply.github.com>
2022-07-05 19:12:47 +0200

    Add preventDefault

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@
 
 function titleClickHandler(event){
   const clickedElement = this;
+  event.preventDefault();
   console.log('Link was clicked!');
 
   /* remove class 'active' from all article links  */
@@ -14,7 +15,7 @@ for(let activeLink of activeLinks){
   
   /* add class 'active' to the clicked link */
 
-  clickedElement.classList.add('active');
+  clickedElement.add('active');
   console.log('clickedElement:', clickedElement);
   
   /* remove class 'active' from all articles */
