055a476dbae0f49f26c308376184eee8635b9cd9
smulczynskasia <107355316+smulczynskasia@users.noreply.github.com>
2022-07-12 20:15:12 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -183,7 +183,7 @@ for(let tag in allTags){
   
   /* [NEW] generate code of a link and add it to allTagsHTML */
   
-  allTagsHTML += tag + ' (' + allTags[tag] + ');
+  allTagsHTML += tag + ' (' + allTags[tag] + ') ';
 
   /* [NEW] END LOOP: for each tag in allTags: */
 }
